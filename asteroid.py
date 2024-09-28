8a02af4ad344b46319f89e768c1f56a35c450322
Caesar-Sec <lyleirvine72@gmail.com>
2024-09-28 17:15:57 +0100

    completed asteroid game

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import *
 
@@ -12,3 +13,23 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius == ASTEROID_MIN_RADIUS:
+            return
+        else:
+            angle = random.uniform(20, 50)
+            v1 = self.velocity.rotate(angle)
+            v2 = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_1.velocity = v1 * 1.2
+            
+            asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_2.velocity = v2 * 1.2
+
+
+                
+
+
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting asteroids!")
@@ -16,10 +18,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(x=(SCREEN_WIDTH/2), y=(SCREEN_HEIGHT/2))
     astroid_field = AsteroidField()
@@ -32,6 +36,15 @@ def main():
 
         for item in updatable:
             item.update(dt)
+        
+        for item in asteroids:
+            if item.collision_check(player) == True:
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if shot.collision_check(item) == True:
+                    item.split()
+        
 
         screen.fill("black")
 
